6a1b1da5763056884b8cbc145e890c9f04f56116
gnanaprakashcheckin <gnanaprakashcheckin@gmail.com>
2020-04-14 19:03:19 +1300

    Create Spring-boot pet clinic project. closes #1

---
+++ src/main/java/al/guru/springframework/alsfgpetclinic/AlSfgPetClinicApplication.java
@@ -0,0 +1,13 @@
+package al.guru.springframework.alsfgpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlSfgPetClinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlSfgPetClinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/al/guru/springframework/alsfgpetclinic/AlSfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package al.guru.springframework.alsfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlSfgPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
