5b8f7639b645839e7a91cb183d9a1aa5be930249
gnanaprakashcheckin <gnanaprakashcheckin@gmail.com>
2020-04-16 00:54:48 +1300

    Removed ID property in save method of AbstractMapService. Handled to create automatic ID. Update Map Impl to manage setting of ID Property #10

--- al-pet-clinic-data/src/main/java/al/guru/springframework/alsfgpetclinic/service/map/AbstractMapService.java
+++ al-pet-clinic-data/src/main/java/al/guru/springframework/alsfgpetclinic/service/map/AbstractMapService.java
@@ -1,13 +1,12 @@
 package al.guru.springframework.alsfgpetclinic.service.map;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import al.guru.springframework.alsfgpetclinic.model.BaseEntity;
 
-public class AbstractMapService<T, ID> {
+import java.util.*;
 
-    protected Map<ID, T> map = new HashMap();
+public class AbstractMapService<T extends BaseEntity, ID extends Long> {
+
+    protected Map<Long, T> map = new HashMap();
 
     Set<T> findAll(){
         return new HashSet<>(map.values());
@@ -17,8 +16,16 @@ public class AbstractMapService<T, ID> {
         return map.get(id);
     }
 
-    T save(ID id, T object){
-        map.put(id, object);
+    T save(T object){
+
+        if(object != null){
+            if(object.getId() == null){
+                map.put(getNextId(), object);
+            }
+            map.put(object.getId(), object);
+        } else {
+            throw new RuntimeException("Object cannot be null");
+        }
         return object;
     }
 
@@ -28,5 +35,20 @@ public class AbstractMapService<T, ID> {
 
     void deleteById(ID id){
         map.remove(id);
+    }
+
+    private Long getNextId(){
+
+        Long nextId = null;
+
+        try{
+            nextId = Collections.max(map.keySet()) + 1;
+        }catch (NullPointerException ex){
+            nextId = 1L;
+        }catch (NoSuchElementException ex){
+            nextId = 1L;
+        }
+
+        return nextId;
     }
 }
