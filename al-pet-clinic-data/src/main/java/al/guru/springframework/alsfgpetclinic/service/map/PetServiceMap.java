00eded2562e389cb231b6e5f595b509418a990a3
gnanaprakashcheckin <gnanaprakashcheckin@gmail.com>
2020-04-15 22:21:28 +1300

    Implement Map Based Services. closes #7

---
+++ al-pet-clinic-data/src/main/java/al/guru/springframework/alsfgpetclinic/service/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package al.guru.springframework.alsfgpetclinic.service.map;
+
+import al.guru.springframework.alsfgpetclinic.model.Pet;
+import al.guru.springframework.alsfgpetclinic.service.PerService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements PerService {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Pet object) {
+         super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
---
+++ al-pet-clinic-data/src/main/java/al/guru/springframework/alsfgpetclinic/service/map/VetServiceMap.java
@@ -0,0 +1,34 @@
+package al.guru.springframework.alsfgpetclinic.service.map;
+
+import al.guru.springframework.alsfgpetclinic.model.Vet;
+import al.guru.springframework.alsfgpetclinic.service.VetService;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements VetService {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void delete(Vet object) {
+         super.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+}
