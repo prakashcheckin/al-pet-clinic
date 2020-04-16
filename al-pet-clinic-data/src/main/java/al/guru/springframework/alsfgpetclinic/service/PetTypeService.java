7f26b50ce422edcf7bc0df7a4a6db09da2eb026a
gnanaprakashcheckin <gnanaprakashcheckin@gmail.com>
2020-04-16 21:34:33 +1300

    Create PetType Map Service Impl. closes #14

---
+++ al-pet-clinic-data/src/main/java/al/guru/springframework/alsfgpetclinic/service/PetTypeService.java
@@ -0,0 +1,6 @@
+package al.guru.springframework.alsfgpetclinic.service;
+
+import al.guru.springframework.alsfgpetclinic.model.PetType;
+
+public interface PetTypeService extends CurdService<PetType, Long> {
+}
