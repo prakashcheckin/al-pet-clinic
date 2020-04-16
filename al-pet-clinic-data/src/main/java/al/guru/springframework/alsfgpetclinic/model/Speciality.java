8088be937ed09ef80dda6b9da0ee148e99f0ecd2
gnanaprakashcheckin <gnanaprakashcheckin@gmail.com>
2020-04-16 19:34:28 +1300

    added specialities, added to vet. closes #12

---
+++ al-pet-clinic-data/src/main/java/al/guru/springframework/alsfgpetclinic/model/Speciality.java
@@ -0,0 +1,14 @@
+package al.guru.springframework.alsfgpetclinic.model;
+
+public class Speciality extends BaseEntity{
+
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
