cce1272e567152398178d6466489f268cbe8e9a5
gnanaprakashcheckin <gnanaprakashcheckin@gmail.com>
2020-04-19 08:41:39 +1300

    Create Spring Data JPA service for Visit. closes #30

--- al-pet-clinic-data/src/main/java/al/guru/springframework/alsfgpetclinic/service/map/VisitMapService.java
+++ al-pet-clinic-data/src/main/java/al/guru/springframework/alsfgpetclinic/service/map/VisitMapService.java
@@ -2,9 +2,11 @@ package al.guru.springframework.alsfgpetclinic.service.map;
 
 import al.guru.springframework.alsfgpetclinic.model.Visit;
 import al.guru.springframework.alsfgpetclinic.service.VisitService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
 
     @Override
---
+++ al-pet-clinic-data/src/main/java/al/guru/springframework/alsfgpetclinic/service/springdatajpa/VisitSDJpaService.java
@@ -0,0 +1,44 @@
+package al.guru.springframework.alsfgpetclinic.service.springdatajpa;
+
+import al.guru.springframework.alsfgpetclinic.model.Visit;
+import al.guru.springframework.alsfgpetclinic.repositories.VisitRepository;
+import al.guru.springframework.alsfgpetclinic.service.VisitService;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class VisitSDJpaService implements VisitService {
+
+    private final VisitRepository visitRepository;
+
+    public VisitSDJpaService(VisitRepository visitRepository) {
+        this.visitRepository = visitRepository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        Set<Visit> visitSet = new HashSet<>();
+        visitRepository.findAll().forEach(visitSet::add);
+        return visitSet;
+    }
+
+    @Override
+    public Visit findById(Long aLong) {
+        return visitRepository.findById(aLong).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit object) {
+        return visitRepository.save(object);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        visitRepository.delete(object);
+    }
+
+    @Override
+    public void deleteById(Long aLong) {
+        visitRepository.deleteById(aLong);
+    }
+}
